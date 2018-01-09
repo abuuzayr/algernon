0997698fdbf29e1dae2c0b53a935c0058e40ca4c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-09 16:41:55 +0800

    Add route variable and resolve to param passed

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductsService {
 
-  constructor(route: ActivatedRoute) { }
+  route: string;
+
+  constructor(route: ActivatedRoute) {
+    route.params.subscribe(p =>
+      this.route = p.id);
+  }
 
 }
