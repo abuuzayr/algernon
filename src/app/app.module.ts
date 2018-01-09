39eadd8afc8568a68e8fd147869968c4bb852aba
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-09 12:39:26 +0800

    Add product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ShopComponent
+    ShopComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
