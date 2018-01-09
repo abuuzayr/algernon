ba23a9839d9bce2f27bd3b7242f48544a218c306
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-09 16:21:46 +0800

    Import activated route to products service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router/src/router_state';
 
 @Injectable()
 export class ProductsService {
 
-  constructor() { }
+  constructor(route: ActivatedRoute) { }
 
 }
